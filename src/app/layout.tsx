c8f7181c3bd4d716bf2ae64d771dea1e0b53b80a
seoyeon5117 <seoyun020207@gmail.com>
2025-05-22 17:36:08 +0900

    chore: initialize project

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import localFont from "next/font/local";
+import type { Metadata } from "next";
+import "./globals.css";
+
+const pretendard = localFont({
+  src: "./fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+  variable: "--font-pretendard",
+});
+
+export const metadata: Metadata = {
+  title: "Next Vote",
+  description: "Next Vote 21th by Team Influy",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className={pretendard.className}>{children}</body>
+    </html>
+  );
+}
