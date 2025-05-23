837eb684d3c2c8c0dc9a24fa7a7ab5e49a7e46e2
seoyeon5117 <seoyun020207@gmail.com>
2025-05-24 03:17:05 +0900

    design: remove w-full, h-full from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko" className="flex flex-1 w-full h-full">
+    <html lang="ko" className="flex flex-1">
       <body className={`${pretendard.className}`}>{children}</body>
     </html>
   );
