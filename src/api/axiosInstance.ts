8d2c8e635676d363d908b16575fe1268a28245cb
seoyeon5117 <seoyun020207@gmail.com>
2025-05-24 22:28:29 +0900

    feat: install axios and set axiosInstance

---
+++ src/api/axiosInstance.ts
@@ -0,0 +1,37 @@
+import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY } from "@/constants/api";
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
+
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+const getAccessToken = (): string | null => {
+  if (typeof window !== "undefined") {
+    return localStorage.getItem(ACCESS_TOKEN_KEY);
+  }
+  return null;
+};
+
+const getRefreshToken = (): string | null => {
+  if (typeof window !== "undefined") {
+    return localStorage.getItem(REFRESH_TOKEN_KEY);
+  }
+  return null;
+};
+
+const createAxiosInstance = (): AxiosInstance => {
+  const axiosInstace = axios.create({
+    baseURL: BASE_URL,
+    timeout: 10000, // 10초 기다려도 응답 없으면 타임아웃
+    headers: {
+      Authorization: getAccessToken()
+        ? `Bearer ${getAccessToken()}`
+        : undefined,
+    },
+  });
+  // 토큰 리프레시 요청
+  // axiosInstace.interceptors.request.use(
+
+  // )
+  return axiosInstace;
+};
+
+export const authApi = createAxiosInstance();
