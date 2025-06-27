0515838d1cb7daf51302baa50c7d857e1a5c44f0
seoyeon5117 <seoyun020207@gmail.com>
2025-06-27 17:17:37 +0900

    fix: update constants for parts and teams for API interaction

--- src/constants/Teams.ts
+++ src/constants/Teams.ts
@@ -1,9 +1,3 @@
-const TEAMS = [
-  "인플루이",
-  "프로메사",
-  "이어드림",
-  "팝업사이클",
-  "하니홈",
-] as const;
+const TEAMS = ["INFLUY", "HANIHOME", "DEARDREAM", "LOOPZ", "PROMESA"] as const;
 
 export default TEAMS;
